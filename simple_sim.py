fa1625f9508ab47bf710eed6ee6d963cc7aefc42
jamesbolus <james.abolus@outlook.com>
2025-08-09 19:08:33 +0100

    Update simple_sim.py

--- simple_sim.py
+++ simple_sim.py
@@ -36,6 +36,10 @@ for score in target3:
                 elif score-(3*i) == aim:
                     target3[score] = (i, 'T')
 
+# Set scores greater than 171 to just "score" (e.g. either t20 or t19)
+for i in range(171, 502):
+    target3[i] = ("score", "T")
+
 # Define dictonaries for 1 and 2 darts in hand
 target2 = target3
 target1 = target3
@@ -46,5 +50,61 @@ for score in target2:
     for i in range(18, 21):
         if score-i in bull_check:
             target2[score] = (i, 'T')
+
+
+
+# Define function that simulates a player's throw
+
+def throw(score, inhand, player):
+    checkout = 0
+    original_score = score
+    while inhand > 0 and checkout == 0:
+        if inhand == 3:
+            target_number, target_type = target3.get(score, (20, 'T'))
+        elif inhand == 2:
+            target_number, target_type = target2.get(score, (20, 'T'))
+        elif inhand == 1:
+            target_number, target_type = target1.get(score, (20, 'T'))
+        elif inhand == 0:
+            return(score, inhand, checkout)
+        else:
+            print("Error, inhand is not in range")
+    
+        r = random.random()
+        p_switch = 0.25
+        if target_type == 'T':
+            if target_number == "score":
+                r2 = random.random()
+                if r2 < p_switch:
+                    target_number = 19
+                else:
+                    target_number = 20
+            if r < player['trebs'][target_number]:
+                score -= target_number*3
+            else:
+                score -= target_number
+            inhand -= 1
+                
+        elif target_type == 'D':
+            if r < player['dbls'][target_number]:
+                score -= target_number*2
+                if score == 0:
+                    checkout = 1
+            else:
+                r2 = random.random()
+                change = target_number if r2 > 0.5 else 0
+                score -= change
+                if score == 1:
+                    score = 2
+            inhand -= 1
+    
+        elif target_type == 'S':
+            score -= target_number
+            inhand -= 1
+            
+        if score < 0 or score == 1:
+            return (original_score, 0, 0)
+            
+    return(score, inhand, checkout)
 
 
